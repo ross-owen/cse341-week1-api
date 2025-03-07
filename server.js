598713923e1638f850d0bc78879a62c0733cd17f
Ross L. Owen <rowen@byupathway.edu>
2025-03-06 18:49:58 -0700

    week1 lesson2

---
+++ server.js
@@ -0,0 +1,14 @@
+﻿const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Welcome to the server!');
+});
+
+app.get('/ross', (req, res) => {
+    res.send('Ross was here!');
+});
+
+const port = 3000;
+app.listen(process.env.port || port);
+console.log(`Server running on port ${port}`);
